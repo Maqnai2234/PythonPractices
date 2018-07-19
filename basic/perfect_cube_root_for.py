b3fda65b2315af1d0adff193974187165ffa4013
Alexei <alexeim763@gmail.com>
2018-07-19 00:18:05 -0500

    add range data

---
+++ basic/perfect_cube_root_for.py
@@ -0,0 +1,15 @@
+#Find the cube root of a perfect cube with for loop
+x = int(input('Enter an integer: ')) 
+ans = 0
+
+for ans in range(0, abs(x)+1):
+  if ans**3 >= abs(x):
+    break
+
+if ans**3 != abs(x):
+  print(x, 'is not a perfect cube')
+else:
+  if x < 0:
+    ans = -ans
+  print('Cube root of', x , 'is', ans)
+    
